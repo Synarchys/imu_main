2f6a93e9805cb469b9b8558e5c7d721ea3c6e9cf
Ivan Florentin <ivan@sinergetica.com>
2018-08-16 17:50:14 -0400

    navigation

---
+++ src/navigation.nim
@@ -0,0 +1,32 @@
+include karax / prelude 
+import karax / prelude
+
+
+import sugar, json, strutils, macros, jsffi, async, tables
+
+import db
+
+var console {. importc, nodecl .}: JsObject
+
+var myDB {.exportc.} = createDB("myDB", PouchOptions(adapter:"idb"))
+myDB.info().then(proc(r: JsObject ) = console.log("DB_Info:", r))
+
+myDB.destroyDB()
+#console.log(resp)
+
+proc navigation(): VNode =
+  result = buildHtml(nav(class="navbar navbar-expand-lg navbar-light bg-light")):
+    text "nav bar"
+    
+proc content(): VNode =
+  result = buildHtml(tdiv(class="card")):
+    tdiv(class="card-body"):
+      h3(class="card-title"): text "card title"
+      h6(class="card-subtitle mb2 text-muted"): text "Subtitle"
+      
+proc Navigation*(): VNode =
+  result = buildHtml(tdiv()):
+    navigation()
+    for i in 1..20:
+      content()
+
